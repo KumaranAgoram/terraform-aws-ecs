5440b95709ada44dcbca4758df79e4ebfc32f5fa
KumaranAgoram <68787542+KumaranAgoram@users.noreply.github.com>
2023-06-25 21:20:20 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -67,8 +67,8 @@ resource "aws_ecs_task_definition" "task_def" {
       "essential": true,
       "portMappings": [
         {
-          "containerPort": 8082,
-          "hostPort": 8082
+          "containerPort": 8080,
+          "hostPort": 8080
         }
       ],
       "memory": 3072,
